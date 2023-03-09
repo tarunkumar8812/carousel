9fa8af26407f20372c54ee8c2ca092d0934b2063
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-09 23:57:22 +0530

    2 carousel are added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import Carousel1 from './components/carousel1/Carousel1';
+import Carousel2 from './components/carousel2/Carousel2';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is carousel2</h1>
+      <div>  <Carousel2 /> </div>
+      <h1>this is carousel1</h1>
+      {/* <div>  <Carousel1 /> </div> */}
+
+    </div >
+  );
+}
+
+export default App;
