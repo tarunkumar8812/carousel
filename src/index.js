9e1346a0b68c1688040316ddf7c4f083767c917d
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-11-17 16:07:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+const express = require('express');
+const route = require('./routes/route.js')
+const moment = require("moment")
+const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
+require("dotenv").config()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL || "mongodb+srv://TarunKumar123:xLcX9W1SI9646ftM@cluster1.tpwtwiv.mongodb.net/Project_3", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use(
+    function (req, res, next) {
+        let time = moment().format("DD/MM/YYYY hh:mm:ss a")
+        console.log(`time : ${time} , url : ${req.url} `);
+        next();
+    }
 );
+app.use(cors())
+
+app.use('/', route);
+
+app.listen((process.env.PORT || 5000), function () {
+    console.log('Express app running on port ' + (process.env.PORT || 5000))
+});
 
